4126b9a7fe9b96ede9663ab60ffd609ad7fb8bc7
norbertfarkas86 <farkasnorbert86@gmail.com>
2018-09-16 00:30:24 +0200

    v3 update

---
+++ content/script/script.js
@@ -0,0 +1,398 @@
+
+var span = document.getElementsByClassName("close");
+var slate = document.getElementsByClassName("slate");
+var modal = document.getElementsByClassName("modal");
+
+
+
+
+slate[0].onclick = function () {
+    modal[0].style.display = "block";
+}
+
+
+span[0].onclick = function () {
+    modal[0].style.display = "none";
+}
+
+slate[1].onclick = function () {
+    modal[1].style.display = "block";
+}
+
+span[1].onclick = function () {
+    modal[1].style.display = "none";
+}
+
+
+slate[2].onclick = function () {
+    modal[2].style.display = "block";
+}
+
+span[2].onclick = function () {
+    modal[2].style.display = "none";
+}
+
+slate[3].onclick = function () {
+    modal[3].style.display = "block";
+}
+
+span[3].onclick = function () {
+    modal[3].style.display = "none";
+}
+
+
+slate[4].onclick = function () {
+    modal[4].style.display = "block";
+}
+
+span[4].onclick = function () {
+    modal[4].style.display = "none";
+}
+
+
+slate[5].onclick = function () {
+    modal[5].style.display = "block";
+}
+
+
+span[5].onclick = function () {
+    modal[5].style.display = "none";
+}
+
+
+slate[6].onclick = function () {
+    modal[6].style.display = "block";
+}
+
+span[6].onclick = function () {
+    modal[6].style.display = "none";
+}
+
+slate[7].onclick = function () {
+    modal[7].style.display = "block";
+}
+
+
+span[7].onclick = function () {
+    modal[7].style.display = "none";
+}
+
+
+
+span[8].onclick = function () {
+    modal[8].style.display = "none";
+}
+
+
+slate[8].onclick = function () {
+    modal[8].style.display = "block";
+}
+
+
+span[9].onclick = function () {
+    modal[9].style.display = "none";
+}
+
+
+slate[9].onclick = function () {
+    modal[9].style.display = "block";
+}
+
+
+span[9].onclick = function () {
+    modal[9].style.display = "none";
+}
+
+
+slate[10].onclick = function () {
+    modal[10].style.display = "block";
+}
+
+
+span[10].onclick = function () {
+    modal[10].style.display = "none";
+}
+
+
+slate[11].onclick = function () {
+    modal[11].style.display = "block";
+}
+
+
+span[11].onclick = function () {
+    modal[11].style.display = "none";
+}
+
+
+slate[12].onclick = function () {
+    modal[12].style.display = "block";
+}
+
+
+span[12].onclick = function () {
+    modal[12].style.display = "none";
+}
+
+
+slate[13].onclick = function () {
+    modal[13].style.display = "block";
+}
+
+
+span[13].onclick = function () {
+    modal[13].style.display = "none";
+}
+
+
+slate[14].onclick = function () {
+    modal[14].style.display = "block";
+}
+
+
+span[14].onclick = function () {
+    modal[14].style.display = "none";
+}
+
+slate[15].onclick = function () {
+    modal[15].style.display = "block";
+}
+
+
+span[15].onclick = function () {
+    modal[15].style.display = "none";
+}
+
+
+slate[16].onclick = function () {
+    modal[16].style.display = "block";
+}
+
+span[16].onclick = function () {
+    modal[16].style.display = "none";
+}
+
+
+slate[17].onclick = function () {
+    modal[17].style.display = "block";
+}
+
+
+span[17].onclick = function () {
+    modal[17].style.display = "none";
+}
+
+
+slate[18].onclick = function () {
+    modal[18].style.display = "block";
+}
+
+
+span[18].onclick = function () {
+    modal[18].style.display = "none";
+}
+
+
+slate[19].onclick = function () {
+    modal[19].style.display = "block";
+}
+
+
+span[19].onclick = function () {
+    modal[19].style.display = "none";
+}
+
+
+slate[20].onclick = function () {
+    modal[20].style.display = "block";
+}
+
+
+span[20].onclick = function () {
+    modal[20].style.display = "none";
+}
+
+slate[21].onclick = function () {
+    modal[21].style.display = "block";
+}
+
+span[21].onclick = function () {
+    modal[21].style.display = "none";
+}
+
+
+slate[22].onclick = function () {
+    modal[22].style.display = "block";
+}
+
+
+span[22].onclick = function () {
+    modal[22].style.display = "none";
+}
+
+
+slate[23].onclick = function () {
+    modal[23].style.display = "block";
+}
+
+
+span[23].onclick = function () {
+    modal[23].style.display = "none";
+}
+
+
+slate[24].onclick = function () {
+    modal[24].style.display = "block";
+}
+
+
+span[24].onclick = function () {
+    modal[24].style.display = "none";
+}
+
+
+slate[25].onclick = function () {
+    modal[25].style.display = "block";
+}
+
+
+span[25].onclick = function () {
+    modal[25].style.display = "none";
+}
+
+
+slate[26].onclick = function () {
+    modal[26].style.display = "block";
+}
+
+
+span[26].onclick = function () {
+    modal[26].style.display = "none";
+}
+
+
+slate[27].onclick = function () {
+    modal[27].style.display = "block";
+}
+
+
+span[27].onclick = function () {
+    modal[27].style.display = "none";
+}
+
+
+slate[28].onclick = function () {
+    modal[28].style.display = "block";
+}
+
+
+span[28].onclick = function () {
+    modal[28].style.display = "none";
+}
+
+slate[29].onclick = function () {
+    modal[29].style.display = "block";
+}
+
+
+span[29].onclick = function () {
+    modal[29].style.display = "none";
+}
+
+slate[30].onclick = function () {
+    modal[30].style.display = "block";
+}
+
+
+span[30].onclick = function () {
+    modal[30].style.display = "none";
+}
+
+slate[31].onclick = function () {
+    modal[31].style.display = "block";
+}
+
+
+span[31].onclick = function () {
+    modal[31].style.display = "none";
+}
+
+slate[32].onclick = function () {
+    modal[32].style.display = "block";
+}
+
+
+span[32].onclick = function () {
+    modal[32].style.display = "none";
+}
+
+slate[33].onclick = function () {
+    modal[33].style.display = "block";
+}
+
+
+span[33].onclick = function () {
+    modal[33].style.display = "none";
+}
+
+slate[34].onclick = function () {
+    modal[34].style.display = "block";
+}
+
+
+span[34].onclick = function () {
+    modal[34].style.display = "none";
+}
+
+slate[35].onclick = function () {
+    modal[35].style.display = "block";
+}
+
+
+span[35].onclick = function () {
+    modal[35].style.display = "none";
+}
+
+slate[36].onclick = function () {
+    modal[36].style.display = "block";
+}
+
+
+span[36].onclick = function () {
+    modal[36].style.display = "none";
+}
+
+slate[37].onclick = function () {
+    modal[37].style.display = "block";
+}
+
+
+span[37].onclick = function () {
+    modal[37].style.display = "none";
+}
+
+slate[38].onclick = function () {
+    modal[38].style.display = "block";
+}
+
+
+span[38].onclick = function () {
+    modal[38].style.display = "none";
+}
+
+slate[39].onclick = function () {
+    modal[39].style.display = "block";
+}
+
+
+span[39].onclick = function () {
+    modal[39].style.display = "none";
+}
+
+slate[40].onclick = function () {
+    modal[40].style.display = "block";
+}
+
+
+span[40].onclick = function () {
+    modal[40].style.display = "none";
+}
